7433a7f1bcc03029138b47014eab2045cc3cb716
Cufee <vkouzin@outlook.com>
2021-12-28 21:31:25 -0500

    fixed broken checks on delete handlers and out-of-style implementation

--- database/driver/delete.go
+++ database/driver/delete.go
@@ -2,29 +2,28 @@ package driver
 
 import (
 	"aftermath.link/repo/logs"
-	"byvko.dev/repo/openmemorydb/database"
 	"byvko.dev/repo/openmemorydb/types"
 	"github.com/boltdb/bolt"
 )
 
-func (d *driver) DeleteMany(databaseName, collection string, options []types.DeleteOptions) (types.OperationResult, error) {
+func (d *driver) DeleteMany(databaseName, collection string, filter types.Filter, limit int) (types.OperationResult, error) {
 	var result types.OperationResult
 	var deletedDocuments []types.Document
 
 	err := d.db.Batch(func(tx *bolt.Tx) error {
 		bucket, err := getCollectionWithUpsert(tx, databaseName, collection, false)
 		if err != nil {
+			result.Error = err.Error()
 			return logs.Wrap(err, "failed to get collection")
 		}
 
-		for _, delete := range options {
-			matched, err := matchWithCursor(bucket, delete.Filter, 1)
-			if err != nil && err != database.ErrNoDocuments {
-				result.Error = err.Error()
-				continue
-			}
+		matched, err := matchWithCursor(bucket, filter, limit)
+		if err != nil {
+			result.Error = err.Error()
+			return err
+		}
 
-			match := matched[0]
+		for _, match := range matched {
 			uuid, ok := match[documentIDFieldName].(string)
 			if !ok {
 				result.Error = "failed to get document uuid"
@@ -40,12 +39,12 @@ func (d *driver) DeleteMany(databaseName, collection string, options []types.Del
 			result.Deleted++
 		}
 
-		return err
+		return nil
 	})
 	result.Data = deletedDocuments
 	return result, err
 }
 
-func (d *driver) DeleteOne(databaseName, collection string, option types.DeleteOptions) (types.OperationResult, error) {
-	return d.DeleteMany(databaseName, collection, []types.DeleteOptions{option})
+func (d *driver) DeleteOne(databaseName, collection string, filter types.Filter) (types.OperationResult, error) {
+	return d.DeleteMany(databaseName, collection, filter, 1)
 }
